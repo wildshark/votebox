474216d116ea1321a31b70b22e9c819c3ff0dc36
Andrew Quaye <andyquayegh@outlook.com>
2021-08-01 07:06:44 +0000

    update

--- admin/template/login.php
+++ admin/template/login.php
@@ -43,25 +43,18 @@
                                     </div>
                                     <form class="user" action="index.php" method="post">
                                         <div class="form-group">
-                                            <input type="email" name="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="email" name="email" class="form-control form-control-user" d="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" name="password" placeholder="Password">
+                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" name="password" placeholder="Password">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                                <label class="custom-control-label" for="customCheck">Remember Me</label>
                                             </div>
                                         </div>
-                                            <button name="submit" value ="login" class="btn btn-primary btn-user btn-block">
-                                                Login
-                                            </button>
-                                       
+                                        <button name="submit" value ="login" class="btn btn-primary btn-user btn-block">Login</button>
                                     </form>
                                     <hr>
                                     <div class="text-center">
