563b2085172e568077eda83a1ac1475e408ed6f5
Andrew Quaye <andyquayegh@outlook.com>
2021-08-26 23:38:37 +0000

    update

--- admin/admin.php
+++ admin/admin.php
@@ -28,6 +28,43 @@ if(!isset($_SESSION['token'])){
         switch($_REQUEST['_route']){
 
             case"dashboard";
+                //voter
+                $sql ="SELECT count(tbl_candidate.candidate_id) as total FROM tbl_candidate";
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+                $res = $stmt->fetch();
+
+                $voters = $res['total'];
+
+                //candi.
+                $sql ="SELECT count(tbl_presidential_candidate.presidential_id) as total FROM tbl_presidential_candidate";
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+                $res = $stmt->fetch();
+
+                $candidate = $res['total'];
+
+                // total
+                $yr = date("Y"); 
+                $sql = "SELECT count(tbl_voter_box.vote_id) AS total_vote, tbl_voter_box.election_id, tbl_election.election_year FROM tbl_voter_box INNER JOIN tbl_election ON tbl_voter_box.election_id = tbl_election.election_id WHERE tbl_election.election_year =:yr GROUP BY tbl_voter_box.election_id";
+                $stmt = $db->prepare($sql);
+                $stmt->bindParam(':yr', $yr);
+                
+                $stmt->execute();
+
+                $res = $stmt->fetch();
+                $total_vote = $res['total_vote'];
+                $election_id = $res['election_id'];
+
+                //chart
+                $sql ="SELECT count(tbl_voter_box.vote_id) AS total_vote, tbl_voter_box.election_id, tbl_presidential_candidate.presidential_name FROM tbl_voter_box INNER JOIN	tbl_presidential_candidate ON tbl_voter_box.candidate_id = tbl_presidential_candidate.presidential_id WHERE	tbl_voter_box.election_id =:election_id GROUP BY tbl_voter_box.election_id, tbl_presidential_candidate.presidential_name";
+                $stmt = $db->prepare($sql);
+                $stmt->bindParam(':election_id', $election_id);
+
+                $stmt->execute();
+
+                $polling_box = $stmt->fetchAll();
+
                 $label =$admin_lang['dash_page'];
                 require('template/dashboard.php');
             break;
@@ -51,6 +88,7 @@ if(!isset($_SESSION['token'])){
             break;
     
             case"candidate";
+                
                 $_SESSION['election_id'] = $_GET['election'];
                 $sql='SELECT tbl_presidential_candidate.* FROM tbl_presidential_candidate WHERE election_id=:election ORDER BY tbl_presidential_candidate.presidential_id DESC LIMIT 0,6'; 
                 $stmt = $db->prepare($sql);
@@ -59,10 +97,11 @@ if(!isset($_SESSION['token'])){
                 $res = $stmt->fetchAll();
                 $label =$admin_lang['candidate_page'];
                 require('template/add.candidate.php');
+
             break;
 
             case"vote-box";
-
+                
                 $sql ="SELECT tbl_voter_box.election_id, tbl_voter_box.candidate_id, count(tbl_voter_box.voter_id) AS total, tbl_presidential_candidate.presidential_name FROM tbl_voter_box INNER JOIN tbl_presidential_candidate ON tbl_voter_box.candidate_id = tbl_presidential_candidate.presidential_id WHERE tbl_voter_box.election_id = :election GROUP BY tbl_voter_box.candidate_id";
                 $stmt = $db->prepare($sql);
                 $stmt->bindParam(':election', $_GET['election']);
@@ -70,10 +109,11 @@ if(!isset($_SESSION['token'])){
                 $res = $stmt->fetchAll();
                 $label =$admin_lang['vote_box_page'];
                 require('template/add.votebox.php');
+
             break;
 
             case"send-vote-result";
-            
+        
                 $sql ="SELECT tbl_candidate.mobile_num FROM tbl_candidate WHERE tbl_candidate.mobile_num NOTNULL";
                 $stmt = $db->prepare($sql);
                 $stmt->execute();
@@ -99,13 +139,10 @@ if(!isset($_SESSION['token'])){
                     $_req['msg'] ="The winner of the 2021 SRC is {$name}";
                     $sms = file_get_contents('https://app9.cf/sms/?'.http_build_query($_req));
                     
-                    
                 }else{
                     echo"No Mobile Number";
                     exit();
                 }
-
-
             break;
     
             default:
